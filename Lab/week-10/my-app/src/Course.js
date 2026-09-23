2b847e0f25636c2afbf5547f2b37974e5dc49076
joyceangeleena21 <joyceangeleenatera07@gmail.com>
2026-09-23 18:40:35 +0530

    Added Week 8 Week 9 and Week 10 lab work

---
+++ Lab/week-10/my-app/src/Course.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Course() {
+  return (
+    <div>
+      <h2>Course Component</h2>
+      <p>Course: Artificial Intelligence and Data Science</p>
+    </div>
+  );
+}
+
+export default Course;
